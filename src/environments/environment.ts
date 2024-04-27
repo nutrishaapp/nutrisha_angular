56ef84883626642386cbecabaa638e25b4ca9905
EhabMamdouh <59305419+EhabMamdouh@users.noreply.github.com>
2024-04-27 15:10:23 +0300

    environment update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,7 @@
 //const baseUrl = 'https://localhost:5033';
 const baseUrl = 'https://dev.api.nutrisha.app';
+// const baseUrl = 'https://api.nutrisha.app';
+
 
 export const environment = {
   production: true,
