ed0424b6ca677a15a28d6d8a702fcb98a6d7a21b
EhabMamdouh <59305419+EhabMamdouh@users.noreply.github.com>
2025-04-18 17:05:28 +0200

    rea- ai

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 // const baseUrl = 'https://localhost:5033';
 const baseUrl = 'https://dev.api.nutrisha.app';
 // const baseUrl = 'https://api.nutrisha.app';
-const openaiApi = 'Bearer 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'
+const openaiApi = ''
 
 
 export const environment = {
