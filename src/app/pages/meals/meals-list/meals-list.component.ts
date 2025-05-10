7fc9c5b23402c971b31bf0f6bb0a6485a83f60db
EhabMamdouh <59305419+EhabMamdouh@users.noreply.github.com>
2025-05-10 22:14:34 +0300

    Added switch ar & en to meal list

--- src/app/pages/meals/meals-list/meals-list.component.ts
+++ src/app/pages/meals/meals-list/meals-list.component.ts
@@ -4,11 +4,12 @@ import { debounceTime, distinctUntilChanged, Observable, Subject } from 'rxjs';
 import { Select, Store } from '@ngxs/store';
 import { Router } from '@angular/router';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
-import { PagedListQueryModel } from '../../../core/shared';
+import { PagedListQueryModel } from '../../../core/shared/models/paged-list-query.model';
 import { MealsActions } from '../../../core/store/meals/meals.action';
 import { MealsState } from '../../../core/store/meals/meals.state';
 import { MealListModel } from '../../../core/meals/models/meal-list.model';
 import { MealType } from "../../../core/meals/models/meal-type.enum";
+
 
 @UntilDestroy()
 @Component({
@@ -26,11 +27,21 @@ export class MealsListComponent implements OnInit {
     'service'
   ];
 
+
+
   mealType = MealType;
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  queryParams = new PagedListQueryModel({});
+  // selectedFilter: string | null;
 
   search$ = new Subject<string>();
   searchWord: string;
+  isEnglish: string = 'false';
+
+  selectedFilter: string | null = null;
+  selectedFilterValue: string = '';
+
+
 
   // Store
   @Select(MealsState.lastPage) meals$: Observable<MealListModel[]>;
@@ -54,16 +65,19 @@ export class MealsListComponent implements OnInit {
   }
 
   loadArticles() {
+    this.prepareQueryParams();
     this.store.dispatch(
-      new MealsActions.LoadMealsPage(this.prepareQueryParams())
+      new MealsActions.LoadMealsPage(this.queryParams)
     );
   }
 
   prepareQueryParams() {
-    return new PagedListQueryModel({
-      pageNumber: this.paginator.pageIndex,
-      pageSize: this.paginator.pageSize,
-      searchWord: this.searchWord,
-    });
+    this.queryParams.pageNumber = this.paginator.pageIndex;
+    this.queryParams.pageSize = this.paginator.pageSize;
+    this.queryParams.searchWord = this.searchWord;
+
+    this.queryParams.customFilters = this.selectedFilter
+      ? new Map<string, any>([[this.selectedFilter, this.selectedFilterValue]])
+      : null;
   }
 }
